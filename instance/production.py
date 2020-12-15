b996fffee10335dc2270067e52cdb774236fb08f
Ansel Rognlie <ansel.rognlie@gmail.com>
2020-12-15 09:21:52 -0800

    Allows all settings to be read from env in production

--- instance/production.py
+++ instance/production.py
@@ -1,4 +1,9 @@
 import os
 
-DEBUG = False
 SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+SECRET_KEY = os.environ['SECRET_KEY']
+TESTING = os.environ.get('TESTING', False)
+DEBUG = os.environ.get('DEBUG', False)
+CSRF_ENABLED = os.environ.get('CSRF_ENABLED', True)
+SQLALCHEMY_TRACK_MODIFICATIONS = os.environ.get('SQLALCHEMY_TRACK_MODIFICATIONS', False)
+ALLOW_IMPERSONATION = os.environ.get('ALLOW_IMPERSONATION', False)
