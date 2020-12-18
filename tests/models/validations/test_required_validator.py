4699daaf81968006fec48841c2ab5344c3d40d76
Ansel Rognlie <ansel.rognlie@gmail.com>
2020-12-18 12:49:19 -0800

    Adds test for None in required validator

--- tests/models/validations/test_required_validator.py
+++ tests/models/validations/test_required_validator.py
@@ -1,25 +1,38 @@
 import pytest
 
 from betsy.errors.validation_error import ValidationError
-from betsy.models.category import Category
 from betsy.models.validations.required_validator import RequiredValidator
 
+class Fields: pass  # pylint: disable=multiple-statements
+
+#pylint: disable=attribute-defined-outside-init
 class TestValidation:
     def test_valid_no_trim(self):
-        category = Category(name='name')
+        record = Fields()
+        record.name = 'name'
         validator = RequiredValidator('name', trim=False)
-        validator(category)
+        validator(record)
 
     def test_invalid_no_trim(self):
-        category = Category(name='')
+        record = Fields()
+        record.name = ''
         validator = RequiredValidator('name', trim=False)
 
         with pytest.raises(ValidationError):
-            validator(category)
+            validator(record)
 
     def test_invalid_with_trim(self):
-        category = Category(name='       ')
+        record = Fields()
+        record.name = '       '
         validator = RequiredValidator('name')
 
         with pytest.raises(ValidationError):
-            validator(category)
+            validator(record)
+
+    def test_none_as_invalid(self):
+        record = Fields()
+        record.name = None
+        validator = RequiredValidator('name')
+
+        with pytest.raises(ValidationError):
+            validator(record)
