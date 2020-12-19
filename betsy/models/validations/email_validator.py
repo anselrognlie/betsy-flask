42d863e5ccd3bc787f0f27c316c009e7b0fbb08c
Ansel Rognlie <ansel.rognlie@gmail.com>
2020-12-19 12:44:03 -0800

    Adds allow_empty to email validation

--- betsy/models/validations/email_validator.py
+++ betsy/models/validations/email_validator.py
@@ -3,16 +3,20 @@ import re
 from ...errors.validation_error import ValidationError
 
 class EmailValidator:
-    def __init__(self, field, trim=True, message = 'must be a valid email'):
+    def __init__(self, field, allow_empty=False, trim=True, message='must be a valid email'):
         self.field = field
         self.trim = trim
         self.message = message
+        self.allow_empty = allow_empty
 
     def __call__(self, instance):
         value = getattr(instance, self.field)
 
         if self.trim:
             value = str(value).strip()
+
+        if not value and self.allow_empty:
+            return
 
         # uses a simple regex strategy from
         # https://stackoverflow.com/questions/8022530/how-to-check-for-valid-email-address
